e1289e10c32dfaa78ba50f74621b976fa46bb87f
Dawid Naraziński <dawid.narazinski@auctane.com>
2026-05-08 11:52:35 +0200

    initial commit

---
+++ Presentation/Program.cs
@@ -0,0 +1,32 @@
+using Application.Configuration;
+using Infrastructure.Configuration;
+using Presentation.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure();
+builder.Services.AddSignalR();
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins(
+                "http://localhost:3000",
+                "http://localhost:3001"
+            )
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
+    });
+});
+
+
+var app = builder.Build();
+
+app.UseRouting();
+
+app.MapHub<ChessHub>("/chesshub");
+
+app.Run();
